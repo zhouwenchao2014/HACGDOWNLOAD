6dd523205b55a03ff46e86f3c8163f4eb59c4678
wenchao zhou <wenchao zhou>
2017-02-09 01:33:47 +0800

    增加多种迅雷hash

--- js/function.js
+++ js/function.js
@@ -20,5 +20,25 @@ $(function(){
     		} 
     	}
     	$("body")[0].outerHTML=innerText;
+    }else{
+    	var patrn32= new RegExp("[A-Za-z0-9]{32}","g");
+    	result = innerText.match(patrn32);
+    	var length=result.length;
+    	var isContain=false;
+    	for(var i=0;i<length;i++){
+    		value=result[i];
+    		if(source!=null){
+    			for(var index=0;index<source.length;index++){
+    				if(source[index]==value){
+    					isContain=true;
+    				}
+    			}
+    		}
+    		if(!isContain){
+    			source.push(value);
+    			innerText = innerText.replace(value,"<a href='magnet:?xt=urn:btih:"+value+"'>"+value+"</a>");
+    		} 
+    	}
+    	$("body")[0].outerHTML=innerText;;
     }
 });
