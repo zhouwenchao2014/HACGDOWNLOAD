6fc0ccc3e516acb7db5c2c739f2dd58c049f033d
wenchao zhou <wenchao zhou>
2017-02-09 01:27:02 +0800

    修改一个页面多个相同的迅雷hash，出现错误

--- js/function.js
+++ js/function.js
@@ -1,11 +1,23 @@
 $(function(){
 	var innerText = $("body")[0].outerHTML;
 	var patrn= new RegExp("[A-Za-z0-9]{40}","g");
+	var source=[];
     if ((result = innerText.match(patrn)) != null){
     	var length=result.length;
+    	var isContain=false;
     	for(var i=0;i<length;i++){
     		value=result[i];
-    		innerText = innerText.replace(value,"<a href='magnet:?xt=urn:btih:"+value+"'>"+value+"</a>");
+    		if(source!=null){
+    			for(var index=0;index<source.length;index++){
+    				if(source[index]==value){
+    					isContain=true;
+    				}
+    			}
+    		}
+    		if(!isContain){
+    			source.push(value);
+    			innerText = innerText.replace(value,"<a href='magnet:?xt=urn:btih:"+value+"'>"+value+"</a>");
+    		} 
     	}
     	$("body")[0].outerHTML=innerText;
     }
